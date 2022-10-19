c7ab0fe1b743805f11e54f7b1c97a2f19925d9b0
Michael Calabrese <77016291+calabresemic@users.noreply.github.com>
2022-10-19 21:24:57 +0000

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "hello world"
+echo "Setting up repository..."
+
+echo "Installing pre-reqs."
+apt-get update
+apt-get install ca-certificates curl gnupg lsb-release
+
+echo "Adding Docker's official GPG key."
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
