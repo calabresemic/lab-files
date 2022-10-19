f3ffadef8bc41357253dd6492f6d02f51467a1f0
Michael Calabrese <77016291+calabresemic@users.noreply.github.com>
2022-10-19 21:40:32 +0000

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -23,4 +23,4 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
