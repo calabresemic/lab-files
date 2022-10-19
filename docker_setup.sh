b76288070614186451893cc19f12642a7c30529e
Michael Calabrese <77016291+calabresemic@users.noreply.github.com>
2022-10-19 21:38:41 +0000

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,15 +6,21 @@ if (( $EUID != 0 )); then
 fi
 
 echo "Setting up repository..."
-
+echo ""
 echo "Installing pre-reqs."
+echo ""
+
 apt-get update
 apt-get install ca-certificates curl gnupg lsb-release
 
 echo "Adding Docker's official GPG key."
+echo ""
 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
