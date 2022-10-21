704a22a101b3567cefadc21eae3327b645dcfb70
Michael Calabrese <michael_calabrese@ymail.com>
2022-10-20 20:25:39 -0800

    updated docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -36,9 +36,8 @@ echo "Adding Docker's official GPG key"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable' | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #Install docker
 echo "Installing Docker"
